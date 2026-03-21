26aab81c2d15bbd53b016c219459028b17576afa
Phan Ba Song Toan <phanbasongtoan112@gmail.com>
2026-03-21 23:04:46 +0700

    none

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // --- THÊM ĐOẠN BUILD NÀY VÀO ---
   build: {
-    sourcemap: true, 
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('firebase')) return 'firebase';
+            if (id.includes('react')) return 'vendor-react';
+            return 'vendor';
+          }
+        }
+      }
+    }
   },
-  // --------------------------------
   server: {
     host: true,
     port: 5173,
